a3a1468cff9871a6ccf170c1b89ffa09449686da
RafaelPereira014 <rafaelbotelhopereira@ua.pt>
2023-05-25 01:45:58 +0100

    add changes

--- projeto_ICG/icg_project/main.js
+++ projeto_ICG/icg_project/main.js
@@ -4,23 +4,26 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { Mesh } from 'three';
 import { SpotLightHelper } from 'three';
+import sky from '/assets/sky.jpeg';
 
 
 
-const mountain = new GLTFLoader();
+
 const ladyski = new GLTFLoader();
 const snowy = new GLTFLoader();
 const road = new GLTFLoader();
-const snow_rock = new GLTFLoader();
+const wood = new GLTFLoader();
 const snow_ground = new GLTFLoader();
-const snow_house = new GLTFLoader();
+const campfire = new GLTFLoader();
 const cars = new GLTFLoader();
 const tower = new GLTFLoader();
-const neve = new GLTFLoader();
+const finish = new GLTFLoader();
 const rocks = new GLTFLoader();
 const barrier = new GLTFLoader();
-const ramp = new GLTFLoader();
+const tent = new GLTFLoader();
 const sol = new GLTFLoader();
+const wall = new GLTFLoader();
+
 
 
 
@@ -42,6 +45,10 @@ function init(){
  var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
  camera.position.set(0, 10, 50);
 
+
+ const textureLoader = new THREE.TextureLoader();
+scene.background = textureLoader.load(sky);
+
  // camera controls
  
  var renderer = new THREE.WebGLRenderer();
@@ -53,12 +60,16 @@ function init(){
 
  // sun
 
+ 
 
- const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+
+ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambientLight);
   const directionalLight = new THREE.DirectionalLight(0xeead2d, 2)
+    directionalLight.color.set(0xffffff);
+    directionalLight.intensity = 2;
     directionalLight.castShadow = true;
-    directionalLight.angle = 1;
+    directionalLight.angle = 5;
     directionalLight.shadow.mapSize.width = 2048;
     directionalLight.shadow.mapSize.height= 2048;
     directionalLight.position.set(-10,20,-30);
@@ -71,16 +82,8 @@ function init(){
   solTarget.position.set(0,5,0)
   scene.add(solTarget);
   directionalLight.target = solTarget;
-/*
- const spotLight = new THREE.SpotLight(0xffffff, 0.8);
- scene.add(spotLight);
 
-spotLight.angle = 0.8;
-
-spotLight.position.set(0, 25, 0);
-const SpotLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(SpotLightHelper);
-*/
+  
  //snow
  var flakeCount = 20000;
  var flakeGeometry = new THREE.TetrahedronGeometry(0.035); // radius
@@ -118,25 +121,36 @@ scene.add(SpotLightHelper);
  
 
   // Create the mountain
-  mountain.load("assets/mountain/scene.gltf", function(gltf){
-    var mountainModel = gltf.scene;
-    scene.add(mountainModel)
-    mountainModel.scale.set(0.009,0.009,0.009)
-    mountainModel.position.set(-10,-11,-9)
-    mountainModel.rotation.set(0,3.2,0)
-  });
+  // mountain.load("assets/mountain/scene.gltf", function(gltf){
+  //   var mountainModel = gltf.scene;
+  //   scene.add(mountainModel);
+  
+  //   // Traverse the mountain model and set the material color to white
+  //   mountainModel.traverse(function(child){
+  //     if(child.isMesh){
+  //       child.material.color.set(0xffffff);
+  //     }
+  //   });
+  
+  //   mountainModel.scale.set(0.009, 0.009, 0.009);
+  //   mountainModel.position.set(-10, -11, -9);
+  //   mountainModel.rotation.set(0, 3.2, 0);
+  // });
+  
+  
+
 
 
   const skistart = new THREE.Vector3(-25,-13.7,16);
   const skiend = new THREE.Vector3(25,-13.7,16);
   
-  ladyski.load("assets/skiing_lady/scene.gltf", function(gltf){
-    ladyskiModel = gltf.scene;
-    scene.add(ladyskiModel)
-    ladyskiModel.scale.set(1,1,1)
-    ladyskiModel.position.copy(skistart)
-    ladyskiModel.rotation.set(0,1.9,0)
-  });
+  // ladyski.load("assets/skiing_lady/scene.gltf", function(gltf){
+  //   ladyskiModel = gltf.scene;
+  //   scene.add(ladyskiModel)
+  //   ladyskiModel.scale.set(1,1,1)
+  //   ladyskiModel.position.copy(skistart)
+  //   ladyskiModel.rotation.set(0,1.9,0)
+  // });
   document.addEventListener("keydown", onDocumentKeyDown, false);
 
   function onDocumentKeyDown(event) {
@@ -196,8 +210,8 @@ teleferico.load("assets/teleferico/scene.gltf", function(gltf){
    snow_ground.load("assets/patch_of_old_snow/scene.gltf", function(gltf){
     const model = gltf.scene;
     scene.add(model)
-    model.scale.set(100,50,50)
-    model.position.set(-5,-16,-4)
+    model.scale.set(80,50,50)
+    model.position.set(-15,-16,-4)
     model.rotation.set(0,0,0)
    });
 
@@ -205,7 +219,7 @@ teleferico.load("assets/teleferico/scene.gltf", function(gltf){
     const model = gltf.scene;
     scene.add(model)
     model.scale.set(0.015,0.015,0.015)
-    model.position.set(-20,-16.5,-19)
+    model.position.set(-32,-16.5,-19)
     model.rotation.set(0,4,0)
    });
 
@@ -217,8 +231,8 @@ teleferico.load("assets/teleferico/scene.gltf", function(gltf){
     model.rotation.set(0,1.9,0)
    });
 
-
-
+  
+ 
    
 
 
@@ -228,84 +242,174 @@ teleferico.load("assets/teleferico/scene.gltf", function(gltf){
   let m = 15;
   let b = 15;
 
-  barrier.load("assets/psx_style_jersey_barrier/scene.gltf", function(gltf){
-    for (let i = 0; i < 38; i++) {
-        const model = gltf.scene.clone();
-        model.scale.set(1,1,1);
-        const y = b--;
-        model.position.set(y,-13.5,21);
-        model.rotation.set(0, 1.6, 0);
-        models.push(model);
-        scene.add(model);
-    }
-    for (let i = 0; i <48; i++) {
-      const model = gltf.scene.clone();
-      model.scale.set(1,1,1);
-      const y = b++;
-      model.position.set(y,-13.5,21);
-      model.rotation.set(0, 1.6, 0);
-      models.push(model);
-      scene.add(model);
-  }
-});
+//   barrier.load("assets/psx_style_jersey_barrier/scene.gltf", function(gltf){
+//     for (let i = 0; i < 38; i++) {
+//         const model = gltf.scene.clone();
+//         model.scale.set(1,1,1);
+//         const y = b--;
+//         model.position.set(y,-13.5,21);
+//         model.rotation.set(0, 1.6, 0);
+//         models.push(model);
+//         scene.add(model);
+//     }
+//     for (let i = 0; i <48; i++) {
+//       const model = gltf.scene.clone();
+//       model.scale.set(1,1,1);
+//       const y = b++;
+//       model.position.set(y,-13.5,21);
+//       model.rotation.set(0, 1.6, 0);
+//       models.push(model);
+//       scene.add(model);
+//   }
+// });
   
 
 
   road.load("assets/snowy_road/scene.gltf", function(gltf){
-      for (let i = 0; i < 38; i++) {
+      for (let i = 0; i < 58; i++) {
           const model = gltf.scene.clone();
           model.scale.set(2, 2, 2);
           const y = x--;
-          model.position.set(y,-13.5,23);
+          model.position.set(y,-14.5,23);
           model.rotation.set(0, 1.6, 0);
           models.push(model);
           scene.add(model);
       }
-      for (let i = 0; i <48; i++) {
+      for (let i = 0; i <75; i++) {
         const model = gltf.scene.clone();
         model.scale.set(2, 2, 2);
         const y = x++;
-        model.position.set(y,-13.5,23);
+        model.position.set(y,-14.5,23);
         model.rotation.set(0, 1.6, 0);
         models.push(model);
         scene.add(model);
     }
+     let z = 23;
+      for( let i =0; i < 50;i++){
+        const model = gltf.scene.clone();
+        model.scale.set(2.2, 2.2, 2.2);
+        const y = z--;
+        model.position.set(35,-14.5,y);
+        model.rotation.set(0, 0, 0);
+        models.push(model);
+        scene.add(model);
+
+      }
   });
 
-  neve.load("assets/neve_chao/scene.gltf", function(gltf){
-    for (let i = 0; i < 38; i++) {
-        const model = gltf.scene.clone();
-        model.scale.set(0.03,0.03,0.03);
-        const z = m--;
-        model.position.set(z,-14,16);
-        model.rotation.set(0, 1.6, 0);
-        models.push(model);
-        scene.add(model);
+  snowy.load("assets/frozen_lake/scene.gltf", function(gltf){
+    const model = gltf.scene;
+    scene.add(model)
+    model.scale.set(2.5,2.5,2.5)
+    model.position.set(43,-23,-10)
+    model.rotation.set(0,0,0.1)
+  });
+
+   wood.load("assets/wood_platform/scene.gltf", function(gltf){
+    const model = gltf.scene;
+    scene.add(model)
+    model.scale.set(7,7,7)
+    model.position.set(49,-64,-14)
+    model.rotation.set(0,0,0)
+    });
+
+    tent.load("assets/day1_tent/scene.gltf", function(gltf){
+      const model = gltf.scene;
+      scene.add(model)
+      model.scale.set(2,2,2)
+      model.position.set(44,-14.8,10)
+      model.rotation.set(0,-2,0)
+      });
+
+    campfire.load("assets/campfire/scene.gltf", function(gltf){
+      const model = gltf.scene;
+      scene.add(model)
+      model.scale.set(0.2,0.2,0.2)
+      model.position.set(44,-14.5,15)
+      model.rotation.set(0,0,0)
+      });
+
+
+
+
+  
+  
+
+
+ 
+
+cars.load("assets/car_blue/scene.gltf", function(gltf){
+  const model = gltf.scene;
+  scene.add(model)
+  model.scale.set(80,80,80)
+  model.position.set(3,-14.5,23)
+  model.rotation.set(0,0,0)
+
+  // Define variables for car movement
+  let carSpeed = 0.5; // Adjust the speed as needed
+  let carPosition = 0; // Initial position of the car
+
+  function animateCar() {
+    carPosition += carSpeed; // Update the car position
+
+    // Translate the car along the z-axis to move it forward
+    model.position.x += carSpeed;
+
+    // Reset the car position if it goes beyond a certain threshold
+    if (model.position.x > 65) {
+      //model.position.x = -20; // Reset to the initial position
+      carSpeed=0;
+      // You can also randomize the starting position if desired
     }
-    for (let i = 0; i <48; i++) {
-      const model = gltf.scene.clone();
-      model.scale.set(0.03,0.03,0.03);
-      const z = m++;
-      model.position.set(z,-14,16);
-      model.rotation.set(0, 1.6, 0);
-      models.push(model);
-      scene.add(model);
+
+    requestAnimationFrame(animateCar);
   }
+
+  animateCar(); // Start the car animation loop
 });
 
-  cars.load("assets/cars/scene.gltf", function(gltf){
+  finish.load("assets/finish_line/scene.gltf", function(gltf){
     const model = gltf.scene;
     scene.add(model)
     model.scale.set(1,1,1)
-    model.position.set(3,-13.7,23)
-    model.rotation.set(0,3.1,0)
-   });
+    model.position.set(0,-14.5,13)
+    model.rotation.set(0,-1.66,0)
+    });
 
-   
+    
+
+  // mountain.load("assets/mount/scene.gltf", function(gltf){
+  //   const model = gltf.scene;
+  //   scene.add(model)
+  //   model.scale.set(100,100,100)
+  //   model.position.set(-10,0,-20)
+  //   model.rotation.set(0,0,0)
+  //   });
 
 
+   wall.load("assets/wall_of_ice/scene.gltf", function(gltf) {
+    const model = gltf.scene.clone(); // Clone the first model
+    const model2 = gltf.scene.clone(); // Clone the second model
+    const model3 = gltf.scene.clone(); // Clone the third model
+    scene.add(model);
+    scene.add(model2);
+    scene.add(model3);
+    
+    // Set properties for the first model
+    model.scale.set(1, 1, 1);
+    model.position.set(-16, -18, -24);
+    model.rotation.set(0, 0, 0);
+    
+    // Set properties for the second model
+    model2.scale.set(1, 1, 1);
+    model2.position.set(25, -20, -24);
+    model2.rotation.set(0, 0, 0);
 
-
+    // Set properties for the third model
+    model3.scale.set(1, 1, 1);
+    model3.position.set(53, -19, -5);
+    model3.rotation.set(0, -1.5, 0);
+});
 
  // Create houses
  var houseCount = 3;
